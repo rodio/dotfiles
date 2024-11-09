1c4ca3584c636a0ce5a0681c6ef3203606d3408d
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-11-09 20:55:31 +0100

    relative line numbers nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,6 +16,8 @@ vim.g.netrw_liststyle = 3
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
+vim.opt.relativenumber = true
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
